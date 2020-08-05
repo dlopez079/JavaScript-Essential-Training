3372d2f5e755c2b1a8d4505587b6e4eca811bac4
Dlopez079 <dlopez@lopezdavid.dev>
2020-08-05 13:13:45 -0400

    Used pop method to remove last value of the array.

--- 03_08/script.js
+++ 03_08/script.js
@@ -3,14 +3,18 @@ pens = ["red", "blue", "green", "orange"];
 girls = ["Amanda", "Susan", "Vanessa", "Kalise"];
 fellas = ["Edwin", "Bobby", "Lucas", "Matthew", "Pablo"];
 numbers = ["one", "two", "three", "four", "five", "six"];
+linebreak = "";
 
 //Displays the above array;
+console.log(linebreak);
 console.log("Before: ", pens);
 console.log("Before: ", girls);
 console.log("Before", fellas);
 console.log("Before", numbers);
+
 // PROPERTIES:
 // Get a property of an object by name:
+console.log(linebreak);
 console.log("Array length for pens: ", pens.length);
 console.log("Array length for girls: ", girls.length);
 console.log("Array length for fellas: ", fellas.length);
@@ -24,6 +28,7 @@ fellas.reverse();
 numbers.reverse();
 
 //Displays the above array reversed;
+console.log(linebreak);
 console.log("After Reverse: ", pens);
 console.log("After Reverse: ", girls);
 console.log("After Reverse:", fellas);
@@ -36,6 +41,7 @@ fellas.shift();
 numbers.shift();
 
 //Displays the above array shift;
+console.log(linebreak);
 console.log("After Shift: ", pens);
 console.log("After Shift: ", girls);
 console.log("After Shift:", fellas);
@@ -45,21 +51,27 @@ console.log("After Shift: ", numbers);
 pens.unshift("purple", "black");
 girls.unshift("Samantha", "Cortney");
 fellas.unshift("George", "Jeffrey");
+numbers.unshift("1", "2");
 
 //Displays the above array comma-separated;
+console.log(linebreak);
 console.log("After Unshift: ", pens);
 console.log("After Unshift: ", girls);
 console.log("After Unshift:", fellas);
+console.log("After Unshift: ", numbers);
 
 // Remove the last value of the array:
 pens.pop();
 girls.pop();
 fellas.pop();
+numbers.pop();
 
 //Displays the above array after last value was removed;
+console.log(linebreak);
 console.log("After Pop: ", pens);
 console.log("After Pop: ", girls);
 console.log("After Pop: ", fellas);
+console.log("After Pop: ", numbers);
 
 // Add comma-separated list of values to the end of the array:
 pens.push("pink", "prussian blue");
@@ -67,6 +79,7 @@ girls.push("Karina", "Jasmin");
 fellas.push("William", "Bryan");
 
 //Displays the above array after values were added to the end of the array;
+console.log(linebreak);
 console.log("After Push: ", pens);
 console.log("After Push: ", girls);
 console.log("After Push: ", fellas);
@@ -77,6 +90,7 @@ girls.splice(0, 2); //Remove Samantha and Cortney
 fellas.splice(4, 3); //Remove Bobby William Bryan
 
 //Displays the above array after certain values are removed;
+console.log(linebreak);
 console.log("After Splice: ", pens);
 console.log("After Splice: ", girls);
 console.log("After Splice: ", fellas);
