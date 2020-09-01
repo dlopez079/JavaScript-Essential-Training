56991c60ea930f42b1a9726dc40beb4770010421
Dlopez079 <dlopez@lopezdavid.dev>
2020-08-31 22:41:07 -0400

    Added functionality to the start button.  The 'Start Over' button already had an event listener which triggered a start function.  I removed the console log from the start function and re-assigned the interval, reset the timer array, set the timerRunning to false, removed value from the test area, revised the timer on the dom to it's original numbers and set the test wrapper to grey.

--- 08_08/script.js
+++ 08_08/script.js
@@ -4,7 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 var interval;
 var timerRunning = false;
 
@@ -18,19 +18,24 @@ function leadingZero(time) {
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    let currentTime =
+        leadingZero(timer[0]) +
+        ":" +
+        leadingZero(timer[1]) +
+        ":" +
+        leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+    timer[0] = Math.floor(timer[3] / 100 / 60);
+    timer[1] = Math.floor(timer[3] / 100 - timer[0] * 60);
+    timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
-    let originTextMatch = originText.substring(0,textEntered.length);
+    let originTextMatch = originText.substring(0, textEntered.length);
 
     if (textEntered == originText) {
         clearInterval(interval);
@@ -42,7 +47,6 @@ function spellCheck() {
             testWrapper.style.borderColor = "#E95D0F";
         }
     }
-
 }
 
 // Start the timer:
@@ -57,7 +61,13 @@ function start() {
 
 // Reset everything:
 function reset() {
-    console.log("reset button has been pressed!");
+    clearInterval(interval); // Make sure the browser is not running any intervals.
+    interval = null; // When we re-assign setInterval, we are not running another interval with a new index number.
+    timer = [0, 0, 0, 0]; // Reset the timer values back to 0.
+    timerRunning = false; // Reset the timerRunning back to false so timer does not run.
+    testArea.value = ""; // This clears the test area from any text.
+    theTimer.innerHTML = "00:00:00:00"; // This is going to reset the timer on the HMTL back to it's original value.
+    testWrapper.style.borderColor = "grey"; // This is going to reset the border back to grey.
 }
 
 // Event listeners for keyboard input and the reset
