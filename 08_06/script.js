2ed619e7e7d22385602ccff632d1cb3f4118321e
Dlopez079 <dlopez@lopezdavid.dev>
2020-08-31 22:11:41 -0400

    Added border response for spell check in the speed typing application.

--- 08_06/script.js
+++ 08_06/script.js
@@ -4,7 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
-var timer = [0,0,0,0];
+var timer = [0, 0, 0, 0];
 
 // Add leading zero to numbers 9 or below (purely for aesthetics):
 function leadingZero(time) {
@@ -16,18 +16,33 @@ function leadingZero(time) {
 
 // Run a standard minute/second/hundredths timer:
 function runTimer() {
-    let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2]);
+    let currentTime =
+        leadingZero(timer[0]) +
+        ":" +
+        leadingZero(timer[1]) +
+        ":" +
+        leadingZero(timer[2]);
     theTimer.innerHTML = currentTime;
     timer[3]++;
 
-    timer[0] = Math.floor((timer[3]/100)/60);
-    timer[1] = Math.floor((timer[3]/100) - (timer[0] * 60));
-    timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000));
+    timer[0] = Math.floor(timer[3] / 100 / 60);
+    timer[1] = Math.floor(timer[3] / 100 - timer[0] * 60);
+    timer[2] = Math.floor(timer[3] - timer[1] * 100 - timer[0] * 6000);
 }
 
 // Match the text entered with the provided text on the page:
 function spellCheck() {
     let textEntered = testArea.value;
+    let originTextMatch = originText.substring(0, textEntered.length); // The substring method treats the string of text as an array and allows us  specify a section within the text to pull out and use as a substance.
+    if (textEntered == originText) {
+        testWrapper.style.borderColor = "#429890";
+    } else {
+        if (textEntered == originTextMatch) {
+            testWrapper.style.borderColor = "#65CCf3";
+        } else {
+            testWrapper.style.borderColor = "#E95D0F";
+        }
+    }
 
     console.log(textEntered);
 }
@@ -38,7 +53,6 @@ function start() {
     if (textEnterdLength === 0) {
         setInterval(runTimer, 10);
     }
-    console.log(textEnterdLength);
 }
 
 // Reset everything:
