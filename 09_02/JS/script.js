41765552d6bee00bc39a4aba166d4f365bd6a8eb
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-02 13:29:19 -0400

    The social media links at the botton of the webpage do not have a target.  I created a loop that will check for links that do not have a target (Social Media Links) and set the tarket to _blank.

--- 09_02/JS/script.js
+++ 09_02/JS/script.js
@@ -1,1 +1,9 @@
+var extLinks = document.querySelectorAll("a[href^='http']"); // Capture all http and https links in the dom and save it to the extLinks variable.
 
+// Create a for look that will loop through the extlink variable which has all the a hrefs saved in it.
+for (var i = 0; i < extLinks.length; i++) {
+    // Create a for loop that will check for links that do not have a target and set the target to _blank.
+    if (!extLinks[i].hasAttribute("target")) {
+        extLinks[i].setAttribute("target", "_blank");
+    }
+}
