547e2a563e5f305eede704185bd5a0b1f818aff4
Dlopez079 <dlopez@lopezdavid.dev>
2020-09-03 15:24:23 -0400

    I created an object that lists the values for each type.  I matached the type to the image.

--- 10_05/JS/script.js
+++ 10_05/JS/script.js
@@ -1,23 +1,32 @@
 const IMAGES = document.querySelectorAll("img");
 
+// List the sizes in an object.
+const SIZES = {
+    showcase: "100vw",
+    reason: "(max-width: 799px) 100vw, 372px",
+    feature: "(max-width: 799px) 100vw, 558px",
+    story: "(max-width: 799px) 100vw, 670px",
+};
+
 function makeSrcset(imgSrc) {
     let markup = [];
     let width = 400;
 
-    for (let i = 0; i<5; i++) {
+    for (let i = 0; i < 5; i++) {
         markup[i] = imgSrc + "-" + width + ".jpg " + width + "w";
-        width+=400;
+        width += 400;
     }
 
     return markup.join();
 }
 
-for (let i = 0; i<IMAGES.length; i++) {
+for (let i = 0; i < IMAGES.length; i++) {
     let imgSrc = IMAGES[i].getAttribute("src");
-    imgSrc = imgSrc.slice(0,-8);
+    imgSrc = imgSrc.slice(0, -8);
     let srcset = makeSrcset(imgSrc);
     console.log(srcset);
 
     let type = IMAGES[i].getAttribute("data-type");
-    console.log(type);
+    let sizes = SIZES[type];
+    console.log(sizes);
 }
